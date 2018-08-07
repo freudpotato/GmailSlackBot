956d169c61155b8dc6699be6f5ef34482c277984
Jeongseok Kang <freudpotato@gmail.com>
2018-08-08 04:48:09 +0900

    Update titleModify.js

--- titleModify.js
+++ titleModify.js
@@ -1,4 +1,4 @@
 var pageTitle = document.title;
 var cutStartPoint = pageTitle.indexOf("harry@socar.kr")-3;
-var mailTitle = ttl.substring(0, cutStartPoint);
+var mailTitle = pageTitle.substring(0, cutStartPoint);
 console.log(mailTitle);
