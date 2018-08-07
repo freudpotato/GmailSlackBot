3d80a515536dbebd9fcc31e88547b672d2821720
Jeongseok Kang <freudpotato@gmail.com>
2018-08-08 05:27:03 +0900

    Update and rename titleModify.js to Bookmarklet.js

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -1,8 +1,10 @@
+// Get mail title from Gmail. I wanted to implement regex but couldn't.
 var pageTitle = document.title;
 var cutStartPoint = pageTitle.indexOf("harry@socar.kr")-3;
 var mailTitle = pageTitle.substring(0, cutStartPoint);
 console.log(mailTitle);
 
-//bookmarklet example thanks to https://gist.github.com/stefanmaric/2abf96c740191cda3bc7a8b0fc905a7d
+// Bookmarklet example thanks to: https://gist.github.com/stefanmaric/2abf96c740191cda3bc7a8b0fc905a7d
+// Copies the mail title to clipboard and pops an alert.
 
 javascript:!function(a){var b=document.createElement("textarea"),c=document.getSelection();b.textContent=a,document.body.appendChild(b),c.removeAllRanges(),b.select(),document.execCommand("copy"),c.removeAllRanges(),document.body.removeChild(b),var pageTitle = document.title,var cutStartPoint = pageTitle.indexOf("harry@socar.kr")-3,var mailTitle = pageTitle.substring(0, cutStartPoint)}(document.title),alert("메일 제목이 복사되었습니다.\n슬랙에서 /gmail (메일제목) 을 입력해보세요.");
